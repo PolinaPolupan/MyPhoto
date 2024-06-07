d65eda954750e8b6db32cb91fec3b8e324b38d9b
PolinaPolupan <polupanpolina9@gmail.com>
2024-06-07 09:48:05 +0300

    FiltersActionsMapping is added to the Command Queue

--- MyPhoto/CommandQueue.cs
+++ MyPhoto/CommandQueue.cs
@@ -9,128 +9,115 @@ namespace MyPhoto
 {
     internal static class CommandQueue
     {
-        public static Dictionary<FiltersLibrary.Filter, int> FiltersValuesMapping = new Dictionary<FiltersLibrary.Filter, int>();
+        private static Dictionary<FiltersLibrary.Filter, int> filtersValuesMapping = new Dictionary<FiltersLibrary.Filter, int>();
+        private static Dictionary<FiltersLibrary.Filter, Func<int, float[][]>> filtersActionsMapping = new Dictionary<FiltersLibrary.Filter, Func<int, float[][]>>();
 
         public static float[][] ApplyAll()
         {
             float[][] matrix = MathUtils.Identity5x5;
-            foreach (KeyValuePair<FiltersLibrary.Filter, int> entry in FiltersValuesMapping)
+            foreach (KeyValuePair<FiltersLibrary.Filter, int> entry in filtersValuesMapping)
             {
-                switch (entry.Key)
-                {
-                    case FiltersLibrary.Filter.BRIGHTNESS:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetBrightnessMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.CONTRAST:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetContrastMatrix(entry.Value), matrix); 
-                        break;  
-                    case FiltersLibrary.Filter.RED:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetRedChannelMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.GREEN:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetGreenChannelMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.BLUE:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetBlueChannelMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.SEPIA:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetSepiaMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.GRAYSCALE:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetGrayscaleMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.NEGATIVE:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetNegativeMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.SATURATION:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetSaturationMatrix(entry.Value), matrix); 
-                        break;
-                    case FiltersLibrary.Filter.TRANSPARENCY:
-                        matrix = MathUtils.Multiply(FiltersLibrary.GetTransparencyMatrix(entry.Value), matrix);
-                        break;
-                }
+                var res = filtersActionsMapping[entry.Key].Invoke(entry.Value);
+                matrix = MathUtils.Multiply(res, matrix);
             }
             return matrix;
         }
 
         public static void AddFilterCommand(FiltersLibrary.Filter filter, int value)
         {
-            FiltersValuesMapping[filter] = value;
+            filtersValuesMapping[filter] = value;
+        }
+
+        private static void AddFunctionCommand(FiltersLibrary.Filter filter, Func<int, float[][]> func)
+        {
+            filtersActionsMapping[filter] = func;
         }
 
         public static Dictionary<FiltersLibrary.Filter, int> GetCommandState()
         {
-            return new Dictionary<FiltersLibrary.Filter, int>(FiltersValuesMapping);
+            return new Dictionary<FiltersLibrary.Filter, int>(filtersValuesMapping);
         }
 
         public static void UpdateCommandState(Dictionary<FiltersLibrary.Filter, int> values)
         {
-            FiltersValuesMapping = values;
+            filtersValuesMapping = values;
         }
 
         public static int GetBrightness()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.BRIGHTNESS];
+            return filtersValuesMapping[FiltersLibrary.Filter.BRIGHTNESS];
         }
 
         public static int GetContrast()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.CONTRAST];
+            return filtersValuesMapping[FiltersLibrary.Filter.CONTRAST];
         }
 
         public static int GetSaturation()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.SATURATION];
+            return filtersValuesMapping[FiltersLibrary.Filter.SATURATION];
         }
 
         public static int GetRed()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.RED];
+            return filtersValuesMapping[FiltersLibrary.Filter.RED];
         }
 
         public static int GetGreen()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.GREEN];
+            return filtersValuesMapping[FiltersLibrary.Filter.GREEN];
         }
 
         public static int GetBlue()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.BLUE];
+            return filtersValuesMapping[FiltersLibrary.Filter.BLUE];
         }
 
         public static int GetSepia()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.SEPIA];
+            return filtersValuesMapping[FiltersLibrary.Filter.SEPIA];
         }
 
         public static int GetGrayscale()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.GRAYSCALE];
+            return filtersValuesMapping[FiltersLibrary.Filter.GRAYSCALE];
         }
 
         public static int GetNegative()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.NEGATIVE];
+            return filtersValuesMapping[FiltersLibrary.Filter.NEGATIVE];
         }
 
         public static int GetTransparency()
         {
-            return FiltersValuesMapping[FiltersLibrary.Filter.TRANSPARENCY];
+            return filtersValuesMapping[FiltersLibrary.Filter.TRANSPARENCY];
         }
 
         public static void ResetAll()
         {
-            FiltersValuesMapping.Clear();
+            filtersValuesMapping.Clear();
             AddFilterCommand(FiltersLibrary.Filter.BRIGHTNESS, 0);
             AddFilterCommand(FiltersLibrary.Filter.CONTRAST, 100);
             AddFilterCommand(FiltersLibrary.Filter.SATURATION, 100);
             AddFilterCommand(FiltersLibrary.Filter.RED, 100);
-            AddFilterCommand(FiltersLibrary.Filter.BLUE, 100);
             AddFilterCommand(FiltersLibrary.Filter.GREEN, 100);
+            AddFilterCommand(FiltersLibrary.Filter.BLUE, 100);            
             AddFilterCommand(FiltersLibrary.Filter.SEPIA, 0);
             AddFilterCommand(FiltersLibrary.Filter.GRAYSCALE, 0);
             AddFilterCommand(FiltersLibrary.Filter.NEGATIVE, 0);
             AddFilterCommand(FiltersLibrary.Filter.TRANSPARENCY, 0);
+
+            filtersActionsMapping.Clear();
+            AddFunctionCommand(FiltersLibrary.Filter.BRIGHTNESS, FiltersLibrary.GetBrightnessMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.CONTRAST, FiltersLibrary.GetContrastMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.SATURATION, FiltersLibrary.GetSaturationMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.RED, FiltersLibrary.GetRedChannelMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.GREEN, FiltersLibrary.GetGreenChannelMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.BLUE, FiltersLibrary.GetBlueChannelMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.SEPIA, FiltersLibrary.GetSepiaMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.GRAYSCALE, FiltersLibrary.GetGrayscaleMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.NEGATIVE, FiltersLibrary.GetNegativeMatrix);
+            AddFunctionCommand(FiltersLibrary.Filter.TRANSPARENCY, FiltersLibrary.GetTransparencyMatrix);
         }
     }
 }
